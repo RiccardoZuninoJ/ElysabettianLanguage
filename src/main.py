3aa0020e6c63e756eaef4a68ae60f089acdfc5c4
RiccardoZuninoJ <riccardozunino@icloud.com>
2021-06-25 19:13:11 +0200

    Added command line -s. Now it is mandatory to add a source code path to run

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
 import lexer
 import parsing
+import argparse
 
 def main():
 
+    inputParser = argparse.ArgumentParser()
+    inputParser.add_argument("-s", "--Source", help = "Source code")
+    args = inputParser.parse_args()
+
     #Reads source code from test.lang
     code = ""
-    with open('test.lang', 'r') as file:
-        code = file.read()
 
-    # Lexer
-    # Initialize Lexer class
-    lex = lexer.Lexer(code)
-    tokens = lex.tokenize()
-    parser = parsing.Parser(tokens)
-    parser.parse()
+    if(args.Source):
+
+        with open(args.Source, 'r') as file:
+            code = file.read()
+        if code:
+
+            # Lexer
+            # Initialize Lexer class
+            lex = lexer.Lexer(code)
+            tokens = lex.tokenize()
+            parser = parsing.Parser(tokens)
+            parser.parse()
+        else:
+            print("Error while opening source code.")
+    else:
+        print("!!! You must specify a source code !!!")
 
 main()
